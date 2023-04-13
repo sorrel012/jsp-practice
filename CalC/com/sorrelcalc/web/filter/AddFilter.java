9cf58c9de130be3b9ecd4be0a38b3a622ca290e3
sorrel012 <wain1719@gmail.com>
2023-04-13 23:44:01 +0900

    fix: add umimplment method

--- CalC/com/sorrelcalc/web/filter/AddFilter.java
+++ CalC/com/sorrelcalc/web/filter/AddFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -20,5 +21,11 @@ public class AddFilter implements Filter {
 		
 		chain.doFilter(request, response);
 	}
+
+    @Override
+    public void destroy() {}
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {}
 
 }
