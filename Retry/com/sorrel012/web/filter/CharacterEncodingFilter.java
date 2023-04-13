d2c4fd28bcef18c8cb0f11a0b6fc754c131deebf
sorrel012 <wain1719@gmail.com>
2023-04-13 23:45:45 +0900

    chore: downgrade tomcate version

--- Retry/com/sorrel012/web/filter/CharacterEncodingFilter.java
+++ Retry/com/sorrel012/web/filter/CharacterEncodingFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -20,8 +21,14 @@ public class CharacterEncodingFilter implements Filter {
 		
 		request.setCharacterEncoding("UTF-8");
 		
-		chain.doFilter(request, response); 
+		chain.doFilter(request, response);
 		
 	}
+
+    @Override
+    public void destroy() {}
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {}
 
 }
