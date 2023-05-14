89ef22c7bee6766529af91a825f74dbd785fef50
sorrel012 <wain1719@gmail.com>
2023-05-14 17:36:49 +0900

    feat: Send&get data using doGet()

---
+++ Servlet_parameter/src/main/java/com/servlet/ex/FormGet.java
@@ -0,0 +1,44 @@
+package com.servlet.ex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/formget")
+public class FormGet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       
+       resp.setContentType("text/html; charset=UTF-8");
+       PrintWriter writer = resp.getWriter();
+       
+       String name = req.getParameter("name");
+       String id = req.getParameter("id");
+       String pw = req.getParameter("pw");
+       
+       String[] hobbies = req.getParameterValues("hobby");
+       String subject = req.getParameter("subject");
+       String protocol = req.getParameter("protocol");
+       
+       
+       writer.println("<html><head></head><body>");
+       writer.println("이름 : " + name + "<br>");
+       writer.println("아이디 : " + id + "<br>");
+       writer.println("비밀번호 :" + pw + "<br>");
+       writer.println("취미 : " + Arrays.toString(hobbies) + "<br>");
+       writer.println("좋아하는 과목 : " + subject + "<br>");
+       writer.println("프로토콜 : " + protocol + "<br>");
+       writer.println("</body><html>");
+       
+       
+        
+    }
+    
+}
