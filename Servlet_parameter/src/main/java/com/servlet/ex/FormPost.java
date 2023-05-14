c27eead152e16b24a1fea81d9d1474d4990637fd
sorrel012 <wain1719@gmail.com>
2023-05-14 17:02:24 +0900

    feat: Semd&get Parameter with doPost

---
+++ Servlet_parameter/src/main/java/com/servlet/ex/FormPost.java
@@ -0,0 +1,41 @@
+package com.servlet.ex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/formpost")
+public class FormPost extends HttpServlet {
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       
+       String id = req.getParameter("id");
+       String pw = req.getParameter("pw");
+       
+       String[] hobbies = req.getParameterValues("hobby");
+       String subject = req.getParameter("subject");
+       String protocol = req.getParameter("protocol");
+       
+       resp.setContentType("text/html; charset=UTF-8");
+       PrintWriter writer = resp.getWriter();
+       
+       writer.println("<html><head></head><body>");
+       writer.println("아이디 : " + id + "<br>");
+       writer.println("비밀번호 :" + pw + "<br>");
+       writer.println("취미 : " + Arrays.toString(hobbies) + "<br>");
+       writer.println("좋아하는 과목 : " + subject + "<br>");
+       writer.println("프로토콜 : " + protocol + "<br>");
+       writer.println("</body><html>");
+       
+       
+        
+    }
+    
+}
