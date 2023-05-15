bf528220581750936b8418c65b5b9628669c05c2
sorrel012 <wain1719@gmail.com>
2023-05-15 23:13:16 +0900

    feat: Revise column name

--- JSP_daodto/script.sql
+++ JSP_daodto/script.sql
@@ -2,8 +2,8 @@ create table tblMember (
     name varchar2(20),
     id varchar2(20),
     pw varchar2(20),
-    phone1 varchar2(20),
-    phone2 varchar2(20),
-    phone3 varchar2(20),
+    tel1 varchar2(20),
+    tel2 varchar2(20),
+    tel3 varchar2(20),
     gender varchar2(20)
 );
