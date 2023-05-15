55dab5310a781b564c0ecb2d990dcce696ad743a
sorrel012 <wain1719@gmail.com>
2023-05-15 23:57:23 +0900

    feat: Insert new user

--- JSP_daodto/script.sql
+++ JSP_daodto/script.sql
@@ -2,8 +2,9 @@ create table tblMember (
     name varchar2(20),
     id varchar2(20),
     pw varchar2(20),
-    tel1 varchar2(20),
-    tel2 varchar2(20),
-    tel3 varchar2(20),
+    tel varchar2(50),
     gender varchar2(20)
 );
+
+insert into tblMember values('하니', 'sorrel012', '1234', '010-1234-5678', 'woman');
+insert into tblMember values('정재현', 'jay0214', '0214', '010-7895-4562', 'man');
