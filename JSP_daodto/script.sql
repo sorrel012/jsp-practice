5b43416d5cb99b191167667c6ea34b7735bf426d
sorrel012 <wain1719@gmail.com>
2023-05-15 23:03:32 +0900

    feat: Create DDL

---
+++ JSP_daodto/script.sql
@@ -0,0 +1,9 @@
+create table tblMember (
+    name varchar2(20),
+    id varchar2(20),
+    pw varchar2(20),
+    phone1 varchar2(20),
+    phone2 varchar2(20),
+    phone3 varchar2(20),
+    gender varchar2(20)
+);
